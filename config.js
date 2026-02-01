ae55a7a9439ae26c686a39f8d2ed4a087fabf915
LuisCheats <luiselcrack183@gmail.com>
2026-02-01 03:25:15 -0500

    update

---
+++ config.js
@@ -0,0 +1,22 @@
+
+// token de seu Cluster do mongoDb
+
+const token = 'SEU CLUSTER AQUI'; 
+
+module.exports = {
+token
+}
+
+/* @CLOVERMYT */
+
+// Canal: https://youtube.com/@clovermyt
+
+// Canal WhatsApp: https://whatsapp.com/channel/0029Va974hY2975B61INGX3Q
+
+// Instagram: https://www.instagram.com/clovermods?igsh=MmcyMHlrYnhoN2Zk
+
+// Telegram: t.me/cinco_folhas
+
+// Comunidade WhatsApp: https://chat.whatsapp.com/Kc5HLGCIokb37mA36NJrM6
+
+// SE FOR REPOSTAR ME MARCA 🧙‍♂️🍀
